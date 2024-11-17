be180e86b138ce34e518e71196194129aa5aef19
trackmateai2 <trackmateai2@gmail.com>
2024-11-17 12:12:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function refresh(){
     
         cod=document.getElementById("value1").innerHTML;
         cod2=document.getElementById("value2").innerHTML;
-        alert(cod+","+cod2);
+        alert("My Location: \n"+cod+","+cod2);
         
         var marker = L.marker([cod, cod2]).addTo(map);
         marker.bindPopup("<font color=#000000><b>My Location </b> <br/> <font size=0.5px color=grey>  Accuracy 97%... ").openPopup();   
