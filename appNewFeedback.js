7c06769921f17ee4d076dc6f6efc8d363d178fc5
trackmateai2 <trackmateai2@gmail.com>
2024-11-18 05:09:47 -0700

    Update appNewFeedback.js

--- appNewFeedback.js
+++ appNewFeedback.js
@@ -58,326 +58,326 @@ if (username=="gmasele15@gmail.com"){
 
 
 if (username=="phexmore@gmail.com"){
-           window.location.href=("resultsPhex.html");
+           window.location.href=("Results/resultsPhex.html");
    }
   
 if (username=="freddy@temp.com"){
-           window.location.href=("resultsFreddy.html");
+           window.location.href=("Results/resultsFreddy.html");
    }
 
 if (username=="77814127_2@temp.com"){
-           window.location.href=("results77814-2.html");
+           window.location.href=("Results/results77814-2.html");
    }
   
 if (username=="77814127@temp.com"){
-           window.location.href=("results77814.html");
+           window.location.href=("Results/results77814.html");
    }
 
 if (username=="71768204@temp.com"){
-           window.location.href=("results71768.html");
+           window.location.href=("Results/results71768.html");
    }
 
 if (username=="katloken@temp.com"){
-           window.location.href=("resultsKatloken.html");
+           window.location.href=("Results/resultsKatloken.html");
    }
   
 if (username=="olorato@temp.com"){
-           window.location.href=("resultsOlorato.html");
+           window.location.href=("Results/resultsOlorato.html");
    }
 
 if (username=="72980793@temp.com"){
-           window.location.href=("results72980.html");
+           window.location.href=("Results/results72980.html");
    }
   
 if (username=="76920299@temp.com"){
-           window.location.href=("results76920.html");
+           window.location.href=("Results/results76920.html");
    }
 
 if (username=="72376304@temp.com"){
-           window.location.href=("results72376.html");
+           window.location.href=("Results/results72376.html");
    }
 
 if (username=="mmagokatlego@temp.com"){
-           window.location.href=("resultsMmagoKatlego.html");
+           window.location.href=("Results/resultsMmagoKatlego.html");
    }
   
 if (username=="moitshepimashika23@gmail.com"){
-           window.location.href=("resultsMoitshepi.html");
+           window.location.href=("Results/resultsMoitshepi.html");
    }
 
 if (username=="mogotsi@temp.com"){
-           window.location.href=("resultsMogotsi.html");
+           window.location.href=("Results/resultsMogotsi.html");
    }
 
 if (username=="tidimalotshwenyego@gmail.com"){
-           window.location.href=("resultsTidimaloTshw.html");
+           window.location.href=("Results/resultsTidimaloTshw.html");
    }
   
 if (username=="gerald@temp.com"){
-           window.location.href=("resultsGerald.html");
+           window.location.href=("Results/resultsGerald.html");
    }
 
 if (username=="tinymmane@temp.com"){
-           window.location.href=("resultsTinymmane.html");
+           window.location.href=("Results/resultsTinymmane.html");
    }
   
 if (username=="rasjay@temp.com"){
-           window.location.href=("resultsRasjay.html");
+           window.location.href=("Results/resultsRasjay.html");
    }
 
 if (username=="mbixx@temp.com"){
-           window.location.href=("resultsMbixx.html");
+           window.location.href=("Results/resultsMbixx.html");
    }
 if (username=="kaonec@temp.com"){
-           window.location.href=("resultsKaonec.html");
+           window.location.href=("Results/resultsKaonec.html");
    }
 
 if (username=="71479022@temp.com"){
-           window.location.href=("results71479.html");
+           window.location.href=("Results/results71479.html");
    }
 
 if (username=="lebesani@temp.com"){
-           window.location.href=("resultsLebesani.html");
+           window.location.href=("Results/resultsLebesani.html");
    }
   
 if (username=="shantee@temp.com"){
-           window.location.href=("resultsShantee.html");
+           window.location.href=("Results/resultsShantee.html");
    }
 
 if (username=="balekani@temp.com"){
-           window.location.href=("resultsBalekani.html");
+           window.location.href=("Results/resultsBalekani.html");
    }
   
 if (username=="lebogangtshepo@temp.com"){
-           window.location.href=("resultsLebogang716.html");
+           window.location.href=("Results/resultsLebogang716.html");
    }
 
 if (username=="bnlpmodisa@gmail.com"){
-           window.location.href=("resultsBnlmodisa.html");
+           window.location.href=("Results/resultsBnlmodisa.html");
    }
 
 if (username=="masterman@temp.com"){
-           window.location.href=("resultsMasterman.html");
+           window.location.href=("Results/resultsMasterman.html");
    }
 
 if (username=="twaezah@temp.com"){
-           window.location.href=("resultsTwaezah.html");
+           window.location.href=("Results/resultsTwaezah.html");
    }
 
 if (username=="raymond@temp.com"){
-           window.location.href=("resultsRaymond.html");
+           window.location.href=("Results/resultsRaymond.html");
    }
 if (username=="isaac@temp.com"){
-           window.location.href=("resultsIsaac.html");
+           window.location.href=("Results/resultsIsaac.html");
    }
 
 
 if (username=="patiencebasie@gmail.com"){
-           window.location.href=("resultsPatience.html");
+           window.location.href=("Results/resultsPatience.html");
    }
 
 
 if (username=="ruthie@temp.com"){
-           window.location.href=("resultsRuthie.html");
+           window.location.href=("Results/resultsRuthie.html");
    }
   
 if (username=="moatemo@temp.com"){
-           window.location.href=("resultsMoatemo.html");
+           window.location.href=("Results/resultsMoatemo.html");
    }
 
 if (username=="lebogang@temp.com"){
-           window.location.href=("resultsLebogang.html");
+           window.location.href=("Results/resultsLebogang.html");
    }
 
 if (username=="badumetse@temp.com"){
-           window.location.href=("resultsBadumetse.html");
+           window.location.href=("Results/resultsBadumetse.html");
    }
   
 if (username=="ontlametse@temp.com"){
-           window.location.href=("resultsOntlametse.html");
+           window.location.href=("Results/resultsOntlametse.html");
    }
 
 
 if (username=="ronald@temp.com"){
-           window.location.href=("resultsRonald.html");
+           window.location.href=("Results/resultsRonald.html");
    }
 
 if (username=="76707830@temp.com"){
-           window.location.href=("results76707.html");
+           window.location.href=("Results/results76707.html");
    }
   
 if (username=="nattysarefo@gmail.com"){
-           window.location.href=("resultsNatty.html");
+           window.location.href=("Results/resultsNatty.html");
    }
 
 if (username=="chrispin@temp.com"){
-           window.location.href=("resultsChrispin.html");
+           window.location.href=("Results/resultsChrispin.html");
    }
 if (username=="thatayaone@temp.com"){
-           window.location.href=("resultsThatayaone.html");
+           window.location.href=("Results/resultsThatayaone.html");
    }
   
 if (username=="empyre@temp.com"){
-           window.location.href=("resultsEmpyre.html");
+           window.location.href=("Results/resultsEmpyre.html");
    }
 if (username=="zaids@temp.com"){
-           window.location.href=("resultsZaids.html");
+           window.location.href=("Results/resultsZaids.html");
    }
 if (username=="tidimalo@temp.com"){
-           window.location.href=("resultsTidimalo.html");
+           window.location.href=("Results/resultsTidimalo.html");
    }
 
 if (username=="kgotso@temp.com"){
-           window.location.href=("resultsKgotso.html");
+           window.location.href=("Results/resultsKgotso.html");
    }
 
 if (username=="bakang@temp.com"){
-           window.location.href=("resultsBakang.html");
+           window.location.href=("Results/resultsBakang.html");
    }
 
 if (username=="balesengbobothobane@gmail.com"){
-           window.location.href=("resultsBaleseng.html");
+           window.location.href=("Results/resultsBaleseng.html");
    }
 if (username=="seleka@temp.com"){
-           window.location.href=("resultsSeleka.html");
+           window.location.href=("Results/resultsSeleka.html");
    }
   
 if (username=="princess@temp.com"){
-           window.location.href=("resultsPrincess.html");
+           window.location.href=("Results/resultsPrincess.html");
    }
 
 if (username=="olefile@temp.com"){
-           window.location.href=("resultsOlefile.html");
+           window.location.href=("Results/resultsOlefile.html");
    }
 
 if (username=="bogosi@temp.com"){
-           window.location.href=("resultsBogosi.html");
+           window.location.href=("Results/resultsBogosi.html");
    }
   
 if (username=="thubans@temp.com"){
-           window.location.href=("resultsThubans.html");
+           window.location.href=("Results/resultsThubans.html");
    }
   
 if (username=="dappamphoeng@gmail.com"){
-           window.location.href=("resultsReetsang.html");
+           window.location.href=("Results/resultsReetsang.html");
    }
 if (username=="73295665@temp.com"){
-           window.location.href=("results73295.html");
+           window.location.href=("Results/results73295.html");
    }
   
 if (username=="virgoangel@temp.com"){
-           window.location.href=("resultsVirgo.html");
+           window.location.href=("Results/resultsVirgo.html");
    }
 
 if (username=="kgopiso@temp.com"){
-           window.location.href=("resultsKgopiso.html");
+           window.location.href=("Results/resultsKgopiso.html");
    }
 if (username=="teekells@temp.com"){
-           window.location.href=("resultsTeekells.html");
+           window.location.href=("Results/resultsTeekells.html");
    }
 
 if (username=="71415574@temp.com"){
-           window.location.href=("results71415.html");
+           window.location.href=("Results/results71415.html");
    }
 
 if (username=="magang@temp.com"){
-           window.location.href=("resultsBoemo.html");
+           window.location.href=("Results/resultsBoemo.html");
    }
 if (username=="boemomagang@gmail.com"){
-           window.location.href=("resultsBoemo.html");
+           window.location.href=("Results/resultsBoemo.html");
    }
 
   
 if (username=="74202145@temp.com"){
-           window.location.href=("results74202.html");
+           window.location.href=("Results/results74202.html");
    }
 
 if (username=="kagiso@temp.com"){
-           window.location.href=("resultsBrianKagiso.html");
+           window.location.href=("Results/resultsBrianKagiso.html");
    }
 
 if (username=="kgosimabalane3@gmail.com"){
-           window.location.href=("resultsKgosimabalane.html");
+           window.location.href=("Results/resultsKgosimabalane.html");
    }
   
 if (username=="73834466@temp.com"){
-           window.location.href=("results73834.html");
+           window.location.href=("Results/results73834.html");
    }
 
 if (username=="75756565@temp.com"){
-           window.location.href=("results75756.html");
+           window.location.href=("Results/results75756.html");
    }
   
 if (username=="gaone00@temp.com"){
-           window.location.href=("resultsGaone.html");
+           window.location.href=("Results/resultsGaone.html");
    }
 
 if (username=="tirelo@temp.com"){
-           window.location.href=("resultsTirelo.html");
+           window.location.href=("Results/resultsTirelo.html");
    }
 
 if (username=="dituwe@temp.com"){
-           window.location.href=("resultsDituwe.html");
+           window.location.href=("Results/resultsDituwe.html");
    }
 
 if (username=="matsoga@temp.com"){
-           window.location.href=("resultsMatsoga.html");
+           window.location.href=("Results/resultsMatsoga.html");
    }
 
 if (username=="maitumelo@temp.com"){
-           window.location.href=("resultsMaitumelo.html");
+           window.location.href=("Results/resultsMaitumelo.html");
    }
 if (username=="maitumelolesang@gmail.com"){
-           window.location.href=("resultsMaitumelo.html");
+           window.location.href=("Results/resultsMaitumelo.html");
    }
 
 if (username=="71741714@temp.com"){
-           window.location.href=("results71741714.html");
+           window.location.href=("Results/results71741714.html");
    }
 
 if (username=="73024187@temp.com"){
-           window.location.href=("results73024.html");
+           window.location.href=("Results/results73024.html");
    }
 
 if (username=="mubutale@gov.bw"){
-           window.location.href=("resultsMubutale.html");
+           window.location.href=("Results/resultsMubutale.html");
    }
   
 if (username=="goffy00@temp.com"){
-           window.location.href=("resultsGoffy.html");
+           window.location.href=("Results/resultsGoffy.html");
    }
 
 if (username=="rowland@temp.com"){
-           window.location.href=("resultsRowland.html");
+           window.location.href=("Results/resultsRowland.html");
    }
 if (username=="76184747@temp.com"){
-           window.location.href=("results76184.html");
+           window.location.href=("Results/results76184.html");
    }
 
 if (username=="msiza@temp.com"){
-           window.location.href=("resultsMsiza.html");
+           window.location.href=("Results/resultsMsiza.html");
    }
 
 if (username=="77183700@temp.com"){
-           window.location.href=("results77183.html");
+           window.location.href=("Results/results77183.html");
    }
   if (username=="75351789@temp.com"){
-           window.location.href=("results75351.html");
+           window.location.href=("Results/results75351.html");
    }
   if (username=="71727191@temp.com"){
-           window.location.href=("results71727.html");
+           window.location.href=("Results/results71727.html");
    }
   if (username=="75177295@temp.com"){
-           window.location.href=("resultsMinge.html");
+           window.location.href=("Results/resultsMinge.html");
    }
  
 if (username=="motlogelwajustin@gmail.com"){
-           window.location.href=("resultsMotlogelwa.html");
+           window.location.href=("Results/resultsMotlogelwa.html");
    }
 
   
 if (username=="madouble@temp.com"){
-           window.location.href=("resultsMadouble.html");
+           window.location.href=("Results/resultsMadouble.html");
    }
   
 
